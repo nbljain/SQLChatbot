af0695afcac1dc6a907950a92776a7c3a13ae72f
nitinbljain <34275295-nitinbljain@users.noreply.replit.com>
2025-04-10 03:16:01 +0000

    Restored to '787cab093bca5ad03106dd2fd4a02de5885aeadd'
    Replit-Restored-To: 787cab093bca5ad03106dd2fd4a02de5885aeadd

--- replit.nix
+++ replit.nix
@@ -1,11 +1,5 @@
 {pkgs}: {
   deps = [
-    pkgs.sqlite
-    pkgs.rustc
-    pkgs.pkg-config
-    pkgs.openssl
-    pkgs.libiconv
-    pkgs.cargo
     pkgs.libxcrypt
     pkgs.postgresql
     pkgs.bash
